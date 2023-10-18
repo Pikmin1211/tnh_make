af310680fa62393fc9275de16901c118cc63d9a3
Pikmin1211 <joefarr1211@yahoo.ca>
2023-10-18 18:52:27 -0400

    move tools to submodules

--- tools/eventassembler/.Sources/EAFormattingSuite/Png2Dmp/LZ77Test.hs
+++
@@ -1,6 +0,0 @@
-import Data.ByteString (readFile, writeFile)
-import Prelude hiding (readFile, writeFile)
-import GBALZ77 (gbaLZ77Compress)
-import System.Environment (getArgs)
-
-main = getArgs >>= \x -> fmap gbaLZ77Compress (readFile $ head x) >>= writeFile "a.out"
--- tools/eventassembler/.Sources/EAFormattingSuite/README.md
+++
@@ -1,2 +0,0 @@
-# EAFormattingSuite
-Suite for formatting different kinds of data into an insertable format through Event Assembler for GBAFE ROMhacking
